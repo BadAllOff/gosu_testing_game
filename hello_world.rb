eacbf9f4fd2bf523be616632c756cfe4f2ef6dd0
Shamil BadAllOff <BadAllOffsh@gmail.com>
2015-10-09 13:51:48 +0500

    Initial Commit

---
+++ hello_world.rb
@@ -0,0 +1,23 @@
+require 'gosu'
+
+class HelloWorldGame < Gosu::Window
+  def initialize width=800, height=600, fullscreen=false
+    super
+    self.caption = 'Hello World'
+  end
+
+  def button_down id
+    close if id == Gosu::KbEscap
+  end
+
+  def update
+
+  end
+
+  def draw
+
+  end
+
+end
+
+HelloWorldGame.new.show
